3c45f09edbca3543e712ab4e10cd5f72a3bf57e8
Hongjunior <139459405+Hongjunior@users.noreply.github.com>
2024-02-03 16:30:53 +0900

    홈 추가

---
+++ home.js
@@ -0,0 +1,14 @@
+// Home.js
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home screen in React.</p>
+    </div>
+  );
+}
+
+export default Home;
