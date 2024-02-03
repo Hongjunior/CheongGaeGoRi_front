61722b4ecea98bab8a5be7becf9ce9b9eb54416f
K0seoyoung <koseo12@hufs.ac.kr>
2024-02-03 22:58:41 +0900

    header logo and p

--- cheong_gaegori/src/component/Header.js
+++ cheong_gaegori/src/component/Header.js
@@ -2,6 +2,11 @@ import '../index.css'
 
 export default function Header(){
     return(
-        <p>hello? It's me! Mario!</p>
-    )
+        <div>
+            <span className='logo_container'>
+                <img className='logo_image' src='/cheong_logo.jpeg' alt='cute' />
+            </span>
+            <p>hello? It's me! Mario!</p>
+        </div>
+    );
 }
