1e447cf2abb0ff634f7b5e56033e511ff9cc9afb
K0seoyoung <koseo12@hufs.ac.kr>
2024-02-03 21:41:55 +0900

    h1,h2,h6,a,p 폰트 지정

--- cheong_gaegori/src/App.js
+++ cheong_gaegori/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className='App'>
       <h1>hello, World!</h1>
+      <h2>hello, World!</h2>
       <h6>hello, World!</h6>
+      <a>hello, World!</a>
       <p>hello, World!</p>
     </div>
   );
