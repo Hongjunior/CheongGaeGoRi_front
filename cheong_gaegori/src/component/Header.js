77a21e152aca0ae9a1d8aa8b55a83c29977596f5
Hongjunior <seokjun09@gmail.com>
2024-02-03 23:08:00 +0900

    header 가로로 정렬

--- cheong_gaegori/src/component/Header.js
+++ cheong_gaegori/src/component/Header.js
@@ -1,12 +1,12 @@
-import '../index.css'
+import '../index.css';
 
-export default function Header(){
-    return(
-        <div>
-            <span className='logo_container'>
-                <img className='logo_image' src='/cheong_logo.jpeg' alt='cute' />
-            </span>
-            <p>hello? It's me! Mario!</p>
-        </div>
-    );
+export default function Header() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <span className='logo_container'>
+        <img className='logo_image' src='/cheong_logo.jpeg' alt='cute' />
+      </span>
+      <p>hello? It's me! Mario!</p>
+    </div>
+  );
 }
